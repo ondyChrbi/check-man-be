5b82698c90b6931082dc98b7a6f69320ac1ed3f3
ondyChrbi <ondrej.chrbolka@icloud.com>
2022-04-15 15:51:59 +0200

    Add connecting table between Team and App user entity

---
+++ src/main/kotlin/cz/fei/upce/checkman/domain/AppUserTeam.kt
@@ -0,0 +1,11 @@
+package cz.fei.upce.checkman.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("app_user_team")
+data class AppUserTeam(
+    @Id var id : Long? = null,
+    var appUser: AppUser? = null,
+    var team : Team? = null
+)
