b23221596e935049e63e0cf00c8f74df76f29f4d
ondyChrbi <ondrej.chrbolka@icloud.com>
2022-04-16 12:06:35 +0200

    Add File attachment for Challenge entity

---
+++ src/main/kotlin/cz/fei/upce/checkman/domain/challenge/ChallengeFileAttachment.kt
@@ -0,0 +1,11 @@
+package cz.fei.upce.checkman.domain.challenge
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("challenge_file_attachment")
+data class ChallengeFileAttachment(
+    @Id var id : Long? = null,
+    var challenge: Challenge? = null,
+    var fileAttachment: FileAttachment? = null
+)
