dab8ba4c6d1b557c28a649daf9424219e8b23928
ondyChrbi <ondrej.chrbolka@icloud.com>
2022-04-16 16:09:36 +0200

    Add OpenAPI 3 configuration and documentation

---
+++ src/main/kotlin/cz/fei/upce/checkman/conf/OpenApi3.kt
@@ -0,0 +1,16 @@
+package cz.fei.upce.checkman.conf
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
+
+@OpenAPIDefinition(
+    info = Info(
+        title = "Check man REST API documentation",
+        description = "Official REST documentation",
+        contact = Contact(name = "Ondřej Chrbolka", email = "ondrej.chrbolka@upce.cz"),
+        version = "0.0.1"
+    ), servers = [Server(url = "http://localhost:9001", description = "Local docker services")]
+)
+class OpenApi3
